5fb5db3d8be8bf57018e3d942c6c6fb7646c73a8
wd1 <akmksi211@gmail.com>
2018-03-20 07:02:08 -0400

    ETH not

--- app/models/worker/deposit_coin_address.rb
+++ app/models/worker/deposit_coin_address.rb
@@ -38,16 +38,16 @@ module Worker
         #   end
         # end
       else 
-        # if currency == 'eth'
-        #   address  = CoinRPC[currency].personal_newAccount("")
-        #   open('http://192.169.153.139/cgi-bin/restart.cgi')
-        # else 
+        if currency == 'eth'
+          # address  = CoinRPC[currency].personal_newAccount("")
+          # open('http://192.169.153.139/cgi-bin/restart.cgi')
+        else 
           if currency == 'btcp'
             address  = CoinRPC[currency].getnewaddress("")
           else 
             address  = CoinRPC[currency].getnewaddress("payment")
           end
-        # end
+        end
 
         if payment_address.update address: address
           ::Pusher["private-#{payment_address.account.member.sn}"].trigger_async('deposit_address', { type: 'create', attributes: payment_address.as_json})
